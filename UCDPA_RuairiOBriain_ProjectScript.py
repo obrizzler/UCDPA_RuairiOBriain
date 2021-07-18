d7c6ababa369bd61a19d1d1e5a344b29536780b8
Ruairi O'Briain <obriain20@hotmail.com>
2021-07-18 20:06:30 +0100

    Fifth Commit - examine dataframe (null values & duplicates)

--- UCDPA_RuairiOBriain_ProjectScript.py
+++ UCDPA_RuairiOBriain_ProjectScript.py
@@ -16,19 +16,56 @@ waiters_tips = pd.read_csv('waiter_tips.csv', encoding = 'ISO-8859-1')
 waiters_tips_type = str(type(waiters_tips))
 print("Waiters_tips is a: " + waiters_tips_type)
 
-# Examine the waiters_tips dataframe - Print a list of the column headers
-column_headers = waiters_tips.columns
-print(column_headers)
-# examine content of waiters_tips dataframe - view top 10 & bottom 5 rows
+# examine the waiters_tips dataframe - Print a list of the column headers
+column_headers = list(waiters_tips.columns)
+print("Column Headers is a " + str(type(column_headers)))
+for i in column_headers:
+    print(i)
+# (alternatively for i in waiters_tips.columns)
+
+# examine the broader content & format of waiters_tips dataframe - view top 10 & bottom 5 rows
 print(waiters_tips.head(10))
 print(waiters_tips.tail())
+
 # examine length and shape of waiters_tips dataframe
 dataframe_length = str(len(waiters_tips))
 dataframe_shape = str(waiters_tips.shape)
 print("The waiters_tips dataframe is " + dataframe_length + " rows long.")
 print("Its shape (rows,columns) is: " + dataframe_shape)
+
 # examine data types of each column
 print(waiters_tips.dtypes)
+
+# examine level of the dataset that is the primary key
+print("Total Bill: " + str(len(waiters_tips.total_bill.unique())))
+print("Tip: " + str(len(waiters_tips.tip.unique())))
+print("Sex: " + str(len(waiters_tips.sex.unique())))
+print("Smoker: " + str(len(waiters_tips.smoker.unique())))
+print("Day: " + str(len(waiters_tips.day.unique())))
+print("Time: " + str(len(waiters_tips.time.unique())))
+
+# display unique values in columns with < 10 unique values
+print("Sex: ", waiters_tips.sex.unique())
+print("Smoker: ", waiters_tips.smoker.unique())
+print("Day: ", waiters_tips.day.unique())
+print("Time: ", waiters_tips.time.unique())
+
+# examine data - search for null values
+null_values = waiters_tips.isnull()
+print(null_values)
+# count null values
+null_value_count = waiters_tips.isnull().sum()
+print(null_value_count)
+# there are no missing values that need to be treated
+"""
+had there been any missing values, would use .fillna function
+"""
+
+# Search for duplicate values
+removed_duplicates = waiters_tips.dropna()
+print(waiters_tips.shape, removed_duplicates.shape)
+# there are no duplicate values indicating each row is a valid unique service by the waiter
+
 
 
 
