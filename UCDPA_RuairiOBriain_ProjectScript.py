f30efd6d3d5e51163ea475b210572b74d1690e47
Ruairi O'Briain <obriain20@hotmail.com>
2021-07-18 18:24:27 +0100

    Fourth Commit - examine dataframe (cont'd)

--- UCDPA_RuairiOBriain_ProjectScript.py
+++ UCDPA_RuairiOBriain_ProjectScript.py
@@ -13,16 +13,24 @@ print(os.getcwd())
 # import .csv dataset
 waiters_tips = pd.read_csv('waiter_tips.csv', encoding = 'ISO-8859-1')
 # check type of imported dataset
-print(type(waiters_tips))
+waiters_tips_type = str(type(waiters_tips))
+print("Waiters_tips is a: " + waiters_tips_type)
 
-# examine new waiters_tips dataframe - view top 10 & bottom 5 rows)
+# Examine the waiters_tips dataframe - Print a list of the column headers
+column_headers = waiters_tips.columns
+print(column_headers)
+# examine content of waiters_tips dataframe - view top 10 & bottom 5 rows
 print(waiters_tips.head(10))
 print(waiters_tips.tail())
-# examine length and shape of dataframe
+# examine length and shape of waiters_tips dataframe
 dataframe_length = str(len(waiters_tips))
 dataframe_shape = str(waiters_tips.shape)
 print("The waiters_tips dataframe is " + dataframe_length + " rows long.")
 print("Its shape (rows,columns) is: " + dataframe_shape)
+# examine data types of each column
+print(waiters_tips.dtypes)
+
+
 
 
 
